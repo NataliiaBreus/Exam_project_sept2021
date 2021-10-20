2b1a89617efcaa393f5f412c16b90139899576ff
NataliiaBreus <breusnataly@gmail.com>
2021-10-20 11:52:50 +0300

    deleted dockerfile changes in start-dev and start

--- start.sh
+++ start.sh
@@ -11,16 +11,15 @@ docker-compose  up -d
 echo ""
 docker-compose  ps
 
-web=$(docker-compose ps | grep squadhelp_server-prod_1 | awk '{print $1}')
 
 # Run Sequalize's migrations.
 echo "-----> Running application migrations"
-docker exec -it "$web" sequelize db:migrate
+docker exec -it new-squadhelp_server-prod_1 sequelize db:migrate
 echo ""
 
 # Run Sequalize's seeds.
 echo "-----> Running application seeds"
-docker exec -it "$web" sequelize db:seed:all
+docker exec -it new-squadhelp_server-prod_1 sequelize db:seed:all
 echo "<----- Seeds created"
 
 ended_at=$(date +"%s")
